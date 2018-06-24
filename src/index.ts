444cf66351f42c2f99b3b69dd6f04883c6e9e923
Dominic Lee <d.lee@zailab.com>
2018-06-24 15:01:13 +0200

    Working with imported npm riot-framework

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,53 @@
 import riot from 'riot';
+import riotFramework from 'riot-framework';
 import './views/welcome.tag.html';
 import './main.less';
+import * as uuid from 'uuid';
 
-riot.mount('welcome', {
+let mountedWelcome: any = riot.mount('welcome', {
     title: 'I want to behave!',
-    items: [
-        { title: 'Avoid excessive caffeine', done: true },
-        { title: 'Hidden item',  hidden: true },
-        { title: 'Be less provocative'  },
-        { title: 'Be nice to people' }
-    ]
+    items: ['asdasd']
+})[0];
+
+riotFramework.initialise();
+let welcomeController = riotFramework.createController(mountedWelcome);
+welcomeController.changeProperty({title: 'Hello world'});
+welcomeController.changeProperty({items: ['Well', 'here', 'we', 'are']});
+welcomeController.registerFunction(<FunctionConfig>{
+    key: 'helloworld',
+    callback: (_thing:string) => {
+        console.log(_thing);
+    }
 });
+welcomeController['helloworld']('whasssup');
+riotFramework.initialiseDatabase(<IFirebaseConfig>{
+    apiKey: 'AIzaSyAqVI5xIo_PzZ6aEhz0AWYSWseKKLWB2cw',
+    authDomain: "rad-riotstarter.firebaseapp.com",
+    databaseURL: 'https://rad-riotstarter.firebaseio.com/'
+});
+riotFramework.postToDatabase(<IUserScore>{
+    UserId: uuid.v4(),
+    FirstName: 'Dom',
+    LastName: 'Lee',
+    Score: Math.round(Math.random()*100),
+    TimeStamp: new Date().toUTCString()
+});
+
+interface FunctionConfig {
+    key: string
+    callback: Function
+}
+
+interface IFirebaseConfig {
+    apiKey: string
+    authDomain?: string
+    databaseURL?: string
+}
+
+interface IUserScore {
+    UserId: string
+    FirstName: string
+    LastName: string
+    Score: number
+    TimeStamp: any
+}
